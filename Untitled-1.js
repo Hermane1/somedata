ae7471b28c4f1be9fe854b24623e5d6e94f01224
Hermane1 <essence.herman404@gmail.com>
2024-06-11 16:53:07 -0400

    added ()and re-moved =

---
+++ Untitled-1.js
@@ -0,0 +1,3 @@
+let names = ["Patrick","Brady","Bradon","Christian"];
+let food = ["pizza","hamburger","crawfish","fruit-tart","fried chicken","salad","soup","tacos","sandwich","sushi"];
+
---
+++ storingdata.js
@@ -0,0 +1,6 @@
+let names = ["Patrick","Brady","Bradon","Christian"];
+let food = ["pizza","hamburger","crawfish","fruit-tart","fried chicken","salad","soup","tacos","sandwich","sushi"];
+let state = [["North Carolina","Raleigh","Northen Cardinal"],["Alabama","Yellowhammer","Montgomery"],["Alaska","Willow Ptarmigan","Juneau"],["Araziona","Cactus Wren","Phoniex"],["Idaho","Mountian Bluebird","Boise"],["Louisiana","Brown Pelican","Baton Rouge"],["Nebraska","WesternMedowlark","Lincoln"]];
+console.log (names);
+console.log  (food);
+console.log (state);
